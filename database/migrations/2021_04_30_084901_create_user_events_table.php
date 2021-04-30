23b0b5bc9051f078a9c91c1f6cdfdd3d3aa5be37
spraddles <brett.spradbury@gmail.com>
2021-04-30 19:00:39 +1000

    Create user + event migrations

--- database/migrations/2021_04_30_084901_create_user_events_table.php
+++ database/migrations/2021_04_30_084901_create_user_events_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateUserEventsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('user_events', function (Blueprint $table) {
+            $table->increments('eventId');
+            $table->timestamp('eventDate');
+            $table->string('eventType');
+            $table->string('eventMessage');
         });
     }
 
@@ -27,6 +28,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('user_events');
     }
 }
