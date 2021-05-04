ab9f69ea25642f84f29e3d4912ed50b07703f54f
spraddles <brett.spradbury@gmail.com>
2021-05-04 20:03:56 +1000

    Rename model classes
    Was causing conflicts with Laravel's OOTB models

---
+++ app/Models/UserEventsTable.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserEventsTable extends Model
+{
+    protected $table = 'user_events_table';
+}
---
+++ app/Models/UserTable.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserTable extends Model
+{
+    protected $table = 'user_table';
+}
