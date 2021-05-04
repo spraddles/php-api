65d03e857562762ce4b6af1674808e6c986c198c
spraddles <brett.spradbury@gmail.com>
2021-05-04 21:42:19 +1000

    Remaining API's
    Included Postman file for testing

--- app/Http/Controllers/UserEventsController.php
+++ app/Http/Controllers/UserEventsController.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Query\Builder;
+
 use App\Models\UserEventsTable;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class UserEventsController extends Controller
 {
@@ -15,6 +21,48 @@ class UserEventsController extends Controller
     public function index()
     {
         return UserEventsTable::all();
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function grouped()
+    {
+
+        return UserEventsTable::all()->groupBy('eventType');
+
+        /* needs completing:
+        $grouped = DB::table('user_events_table')
+            ->selectRaw('"userId", "eventType", COUNT(*)')
+            ->groupByRaw('"userId", "eventType"')
+            ->get();*/
+
+        /* we're looking for something like this:
+        "userId": 144 {
+            events: 3 {
+                "eventType": "view" {
+                    "eventId": 243,
+                    "eventMessage": "view product X"
+                    "eventId": 244,
+                    "eventMessage": "view product X"
+                }
+                "eventType": "check" {
+                    "eventId": 245,
+                    "eventMessage": "check product X"
+                    "eventId": 246,
+                    "eventMessage": "check product X"
+                }
+                "eventType": "buy" {
+                    "eventId": 247,
+                    "eventMessage": "buy product X"
+                    "eventId": 248,
+                    "eventMessage": "buy product X"
+                }
+            }
+        }
+        */
     }
 
     /**
